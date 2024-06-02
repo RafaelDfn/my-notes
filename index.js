14b2c9be9fb460e6d4abab4fd9909d219dd154d4
unknown <rafaeldfnrafa@gmail.com>
2024-06-02 11:14:18 -0400

    só consigo adicionar uma nota

---
+++ index.js
@@ -0,0 +1,16 @@
+const nota = document.getElementById("newNote");
+const lugar = document.getElementById('notesPlace');
+// const marcar = document.createElement("input");
+
+
+function add() {
+    lugar.innerHTML = nota.value;
+    // marcar = document.getElementById("cx")
+    
+}
+const jafiz = document.getElementById("tarefasFeitas")
+function notaFeita() {
+    jafiz.innerHTML = nota.value
+    
+
+}
